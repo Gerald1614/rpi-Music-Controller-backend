5e2a6a2e8305d5069f720962cb796ac9b2d204c3
gerald Michelant <gerald.michelant@gmail.com>
2019-09-03 17:15:48 -0400

    initial setup(receive mqtt message and connect to chromecast)

---
+++ readme.md
@@ -0,0 +1,4 @@
+
+RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install paho-mqtt
+RUN pip install pychromecast
---
+++ rpi_audio/tuneIn.py
@@ -0,0 +1,73 @@
+
+import time
+import sys
+import json
+import logging
+import paho.mqtt.client as mqtt
+import pychromecast
+
+global cast
+cast = pychromecast.Chromecast('192.168.2.17')
+cast.wait()
+
+print("{0}" .format(cast.device))
+time.sleep(1)
+print("{0}" .format(cast.status))
+
+if '--show-status-only' in sys.argv:
+    sys.exit()
+if not cast.is_idle:
+    print("Killing current running app")
+    cast.quit_app()
+    time.sleep(5)
+global mc
+mc = cast.media_controller
+
+def cc_action(payload):
+    if payload["action"] == "play":
+        mc.play_media((payload["channel"]), "audio/mp3")
+        time.sleep(2)
+        mc.play()
+        print("=> Playing {0}" .format(cast.media_controller.status.content_id))
+    elif payload["action"] == "volume":
+        print("=> Change volume to "+ str(payload["channel"]))
+        cast.set_volume(payload["channel"])
+    elif payload["action"] == "pause":
+        print("=> Sending pause command")
+        mc.pause()
+    elif payload["action"] == "stop":
+        print("=> Sending stop command")
+        mc.stop()
+    elif payload["action"] == "quit":
+        print("=> Sending quit_app command")
+        cast.quit_app()
+
+#hostname
+broker='localhost'
+#port
+port=1883
+#time to live
+timelive=60
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    client.subscribe("radio_action")
+
+def on_message(client, userdata, msg):
+    if str(msg.topic) == 'radio_action':
+        payload = json.loads(msg.payload)
+        cc_action(payload)
+        print("payload: " + payload["channel"])
+
+def on_subscribe(client, userdata, mid, granted_qos):
+ print("Subscribed to Topic: " + 
+ "radio_action" + " with QoS: " + str(granted_qos))
+
+client = mqtt.Client()
+
+client.on_connect = on_connect
+client.on_message = on_message
+client.on_subscribe = on_subscribe
+
+client.connect(broker, port, timelive)
+client.loop_forever()
